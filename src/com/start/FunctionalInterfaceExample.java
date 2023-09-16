5384e80ea54bfa790cc80f08f244f12391aa7a87
charanraj <charanrajjb007@gmail.com>
2023-09-16 21:57:49 +0530

    small change

--- src/com/start/FunctionalInterfaceExample.java
+++ src/com/start/FunctionalInterfaceExample.java
@@ -6,16 +6,35 @@ interface sayable {
 }
 
 @FunctionalInterface
-interface Doable  {
+interface Doable {
 	// Invalid '@FunctionalInterface' annotation; Doable is not a functional
 	// interface
 	// void doIt();
 	default void defaultfunction() {
 		System.out.println("Default Function");
 	}
+
 	void doit();
 }
 
-public class FunctionalInterfaceExample {
+public class FunctionalInterfaceExample implements sayable, Doable {
+	public static void main(String[] args) {
+		FunctionalInterfaceExample fi=new FunctionalInterfaceExample();
+		fi.say("sayable interface");
+		fi.defaultfunction();
+		fi.doit();
+	}
+
+	@Override
+	public void say(String msg) {
+		System.out.println("sayable Functional interface: "+msg);
+
+	}
+
+	@Override
+	public void doit() {
+		System.out.println("Doable Functional interface");
+
+	}
 
 }
