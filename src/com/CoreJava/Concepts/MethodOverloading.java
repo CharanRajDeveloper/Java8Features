b6b251750da2732f5b99e9ca849eefc55af355e9
charanraj <charanrajjb007@gmail.com>
2023-11-11 20:44:50 +0530

    Demo

--- src/com/CoreJava/Concepts/MethodOverloading.java
+++ src/com/CoreJava/Concepts/MethodOverloading.java
@@ -1,10 +1,10 @@
 package com.CoreJava.Concepts;
 
 public class MethodOverloading {
-	public static void run(int a,int b) {
+	public static void run(int a,int b) {   //two parameters
 		System.out.println("First Method");
 	}
-	public static void run(int a,int b,double c) {
+	public static void run(int a,int b,double c) {  // three parameters
 		System.out.println("Second Method");
 	}
 	public static void main(String[] args) {
